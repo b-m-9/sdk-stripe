509f3545af7601e8810f83b307fd1928492effb7
medve-dev <200.medvedev@gmail.com>
2018-03-20 11:05:35 +0200

    stripe init

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 'use strict';
-
+const install_npm = require('npm-install-package');
+const KEY_PAY = __dirname.split('payments/')[1];
+module.exports.name = 'Stripe';
+module.exports.description = '';
+module.exports.key = KEY_PAY;
+module.exports.currency = ['USD', 'EUR'];
 module.exports.MerchantClass = require('./lib/merchant');
-
 module.exports.ApiClass = require('./lib/api');
 module.exports.config = require('./config');
+module.exports.install = new Promise((resolve, reject) => {
+    install_npm(['stripe'], {}, err => {
+        if (err) return reject(err);
+        return resolve(KEY_PAY);
+    });
+});
